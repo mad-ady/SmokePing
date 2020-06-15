4ed2fb22b436a0bce61a6d2e3ed5ab202b1e0f2e
Robert Loomans <robert@loomans.org>
2020-06-16 08:33:18 +1000

    its a method not a function

--- lib/Smokeping.pm
+++ lib/Smokeping.pm
@@ -4118,7 +4118,8 @@ sub load_cfg ($;$) {
                 timeout => $cfg->{'InfluxDB'}{'timeout'}
             );
             if (defined $cfg->{'InfluxDB'}{'username'} && defined $cfg->{'InfluxDB'}{'password'}) {
-                my $ua = get_lwp_useragent();
+                do_log("DBG: Setting credentials for InfluxDB connection");
+                my $ua = $influx->get_lwp_useragent();
                 $ua->credentials(
                     $cfg->{'InfluxDB'}{'host'} . ':' . $cfg->{'InfluxDB'}{'port'},
                     'InfluxDB',
